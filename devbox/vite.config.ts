bc763d4eb30bc21c8de98a6785102cc9ac74eea8
atian <atian@micous.com>
2025-12-05 18:45:39 +0800

    feat:修改配置路径

--- devbox/vite.config.ts
+++ devbox/vite.config.ts
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  // GitHub Pages 部署路径：/仓库名/子目录/
-  // 例如仓库名是 DJ_TOGO，子目录是 devbox
-  base: process.env.NODE_ENV === 'production' ? '/DJ_TOGO/devbox/' : '/',
+  // GitHub Pages 部署路径：/仓库名/
+  base: process.env.NODE_ENV === 'production' ? '/DJ_TOGO/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
